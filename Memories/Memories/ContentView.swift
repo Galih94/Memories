b2827400ccdafa6b56adcde9d1c75bb070c67397
galih <samudragalih94@gmail.com>
2024-10-08 10:23:21 +0700

    feat(PhotosPicker): create selecte Image using `PhotosPicker`

--- Memories/Memories/ContentView.swift
+++ Memories/Memories/ContentView.swift
@@ -5,16 +5,30 @@
 //  Created by Galih Samudra on 08/10/24.
 //
 
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedItem: PhotosPickerItem?
+    @State private var resultImage: Image?
     var body: some View {
         NavigationStack {
             VStack {
-                ContentUnavailableView("No Memories", systemImage: "photo.badge.plus", description: Text("Insert new memories"))
+                PhotosPicker(selection: $selectedItem) {
+                    if let resultImage {
+                        /// handle show image here
+                    } else {
+                        ContentUnavailableView("No Memories", systemImage: "photo.badge.plus", description: Text("Insert new memories"))
+                    }
+                }
+                .onChange(of: selectedItem, onSelectedNewImage)
             }
         }
         .navigationTitle("Memories")
+    }
+    
+    private func onSelectedNewImage() {
+        /// handle selected new image here
     }
 }
 
