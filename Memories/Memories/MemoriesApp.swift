0d94721e8a3ab66b803dc96b35c3c636ac3dcb22
galih <samudragalih94@gmail.com>
2024-10-08 10:05:20 +0700

    wip initial project

---
+++ Memories/Memories/MemoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriesApp.swift
+//  Memories
+//
+//  Created by Galih Samudra on 08/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MemoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
